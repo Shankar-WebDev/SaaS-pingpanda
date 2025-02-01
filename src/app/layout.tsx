4ac3b28eedbe386d815213fe40c29916fe8cc023
Sankaranarayanan <sankar241101@gmail.com>
2025-02-01 23:10:53 +0530

    creating auth page for Sign-IN and Sign-UP

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next"
 import { Inter } from "next/font/google"
 import { Providers } from "@/components/providers"
 import { EB_Garamond } from "next/font/google"
+import { ClerkProvider } from "@clerk/nextjs"
 import { cn } from "@/utils"
 
 import "./globals.css"
@@ -24,10 +25,14 @@ export default function RootLayout({
   children: React.ReactNode
 }>) {
   return (
-    <html lang="en" className={cn(inter.variable, eb_garamond.variable)}>
-      <body className="font-sans bg-brand-50 text-brand-950 antialiased">
-        <Providers>{children}</Providers>
-      </body>
-    </html>
+    <ClerkProvider>
+      <html lang="en">
+        <body className="min-h-[calc(100vh-1px)] flex flex-col  bg-brand-50 text-brand-950 antialiased">
+          <main className="relative flex-1 flex flex-col">
+          <Providers>{children}</Providers>
+          </main>
+        </body>
+      </html>
+    </ClerkProvider>
   )
 }
--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,43 +1,47 @@
-import { MaxWidthWrapper } from "./max-width-wrapper"
 import Link from "next/link"
-import { SignedOut } from "@clerk/nextjs"
+import { MaxWidthWrapper } from "./max-width-wrapper"
+import { SignOutButton } from "@clerk/nextjs"
 import { Button, buttonVariants } from "./ui/button"
 import { ArrowRight } from "lucide-react"
-export const Navbar = () => {
-  const user = false
+import { currentUser } from "@clerk/nextjs/server"
+
+export const Navbar = async () => {
+  const user = await currentUser()
+
   return (
     <nav className="sticky z-[100] h-16 inset-x-0 top-0 w-full border-b border-gray-200 bg-white/80 backdrop-blur-lg transition-all">
       <MaxWidthWrapper>
         <div className="flex h-16 items-center justify-between">
           <Link href="/" className="flex z-40 font-semibold">
-            Ping <span className="text-brand-700"> Panda</span>
+            Ping<span className="text-brand-700">Panda</span>
           </Link>
+
           <div className="h-full flex items-center space-x-4">
             {user ? (
               <>
-                <SignedOut>
+                <SignOutButton>
                   <Button size="sm" variant="ghost">
-                    Sign Out
+                    Sign out
                   </Button>
-                </SignedOut>
+                </SignOutButton>
+
                 <Link
                   href="/dashboard"
                   className={buttonVariants({
                     size: "sm",
-                    className: "hidden sm:flex items-center gap-1",
+                    className: "flex items-center gap-1",
                   })}
                 >
-                  Dashboard
-                  <ArrowRight className="ml-1.5 size-4" />
+                  Dashboard <ArrowRight className="ml-1.5 size-4" />
                 </Link>
               </>
             ) : (
               <>
                 <Link
-                  href="/dashboard"
+                  href="/pricing"
                   className={buttonVariants({
                     size: "sm",
-                    variant: "ghost"
+                    variant: "ghost",
                   })}
                 >
                   Pricing
@@ -46,20 +50,22 @@ export const Navbar = () => {
                   href="/sign-in"
                   className={buttonVariants({
                     size: "sm",
-                    variant: "ghost"
+                    variant: "ghost",
                   })}
                 >
-                  Sign In
+                  Sign in
                 </Link>
-                <div  className="h-8 w-px bg-gray-200"/>
+
+                <div className="h-8 w-px bg-gray-200" />
+
                 <Link
                   href="/sign-up"
                   className={buttonVariants({
                     size: "sm",
-                    className: 'flex items-center gap-1.5'
+                    className: "flex items-center gap-1.5",
                   })}
                 >
-                  Sign Up<ArrowRight className="size-4"/>
+                  Sign up <ArrowRight className="size-4" />
                 </Link>
               </>
             )}
