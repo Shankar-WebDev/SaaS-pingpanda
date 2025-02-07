140fe1241af087d16d8a55e7256690c4a1042938
Sankaranarayanan <sankar241101@gmail.com>
2025-02-08 02:36:33 +0530

    Create Pricing page and Update sing-in page

--- src/app/(auth)/sign-in/[[...rest]]/page.tsx
+++ src/app/(auth)/sign-in/[[...rest]]/page.tsx
@@ -1,12 +1,19 @@
 "use client"
 
 import { SignIn } from "@clerk/nextjs"
+import { useSearchParams } from "next/navigation"
 
-const Page = () =>{
-    return <div className="w-full flex-1 flex items-center justify-center">
-        <SignIn/>
+const Page = () => {
+  const searchParms = useSearchParams()
+  const intent = searchParms.get("intent")
+
+  return (
+    <div className="w-full flex-1 flex items-center justify-center">
+      <SignIn
+        forceRedirectUrl={intent ? `/dashboard?intent = ${intent}` : "/"}
+      />
     </div>
+  )
 }
-
 
 export default Page
